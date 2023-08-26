a8047902f603d8a56c5d690ae01b999367cad980
Juan Felipe Toro <felipe.toro710@gmail.com>
2023-08-26 16:06:30 -0500

    cambio total

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            //foreign keys
+
+            $table->unsignedBigInteger('career_id');
+            $table->foreign('career_id')->references('id')->on('careers');
+
+
         });
     }
 
